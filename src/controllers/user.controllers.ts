605f157033b61c3f8f34ae833b2953f9cab2a148
dqmdz <daniel.quinterospinto@gmail.com>
2023-11-23 15:59:23 -0300

    Actualizando update

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -35,14 +35,20 @@ export const getUsers = async (req: Request, res: Response) => {
 
 export const updateUser = async (req: Request, res: Response) => {
 
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const user = await User.findOneBy({ id: parseInt(req.params.id) });
+        const user = await User.findOneBy({ id: parseInt(req.params.id) });
 
-    if (!user) return res.status(404).json({ message: 'User does not exist' });
+        if (!user) return res.status(404).json({ message: 'User does not exist' });
 
-    await User.update({ id: parseInt(id) }, req.body);
+        await User.update({ id: parseInt(id) }, req.body);
 
-    return res.sendStatus(204);
+        return res.sendStatus(204);
 
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
 }
