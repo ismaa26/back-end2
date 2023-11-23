0a0b14ce65d3af5b611f7f8b6c22ef76d43b111d
dqmdz <daniel.quinterospinto@gmail.com>
2023-11-23 15:14:54 -0300

    Agregado de create y getAll

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -3,15 +3,32 @@ import { User } from '../entities/User';
 
 export const createUser = async (req: Request, res: Response) => {
 
-    const { firstname, lastname } = req.body;
+    try {
+        const { firstname, lastname } = req.body;
 
-    const user = new User();
-    user.firstname = firstname;
-    user.lastname = lastname;
+        const user = new User();
+        user.firstname = firstname;
+        user.lastname = lastname;
 
-    await user.save();
+        await user.save();
 
-    console.log(user);
+        return res.json(user);
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
 
-    res.send('Hello world');
+}
+
+export const getUsers = async (req: Request, res: Response) => {
+
+    try {
+        const users = await User.find();
+        return res.json(users);
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(400).json({ message: error.message });
+        }
+    }
 }
